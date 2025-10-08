42d65b439a92204e1665dcaaa6d10357a44a8ea4
nikolasfunke <nikfunke1@gmail.com>
2025-10-08 16:53:45 -0300

    feat: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Routes, Route} from "react-router-dom";
 import Login from "./pages/login";
 import EntidadesButtons from "./components/Entity_buttons.tsx";
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/entidades" element={<EntidadesButtons />} />
+      </Routes>
     </div>
   );
 }

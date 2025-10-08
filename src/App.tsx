729e1bf60cb61807d6f3143e5173c7bf03f7bfa1
nikolasfunke <nikfunke1@gmail.com>
2025-10-08 20:33:38 -0300

    feat: add set_color component, entities page and images

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import { Routes, Route} from "react-router-dom";
 import Login from "./pages/login";
-import EntidadesButtons from "./components/Entity_buttons.tsx";
+import Entities from "./pages/entities.tsx";
 
-function App() {
+export default function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/entidades" element={<EntidadesButtons />} />
+        <Route path="/entidades" element={<Entities />} />
       </Routes>
     </div>
   );
 }
 
 
-export default App;

fcbcfc4f7d08deeb21313438b9a134c54f8ce020
nikolasfunke <nikfunke1@gmail.com>
2025-10-16 17:13:38 -0300

    feat: add home page and back ground image

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route} from "react-router-dom";
 import Login from "./pages/login";
 import Entities from "./pages/entities.tsx";
+import Home from "./pages/home.tsx";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/entidades" element={<Entities />} />
+        <Route path="/home/:entityId" element={<Home />}></Route>
       </Routes>
     </div>
   );

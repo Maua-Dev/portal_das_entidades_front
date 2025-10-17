30ba1b460ed3182912730b4c1dd06e086703fe18
nikolasfunke <nikfunke1@gmail.com>
2025-10-17 10:29:52 -0300

    fix: profile icon

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,10 +1,10 @@
-import { BsPerson } from 'react-icons/bs';
+import { BsFillPersonFill } from 'react-icons/bs';
 
 export default function ProfileButton() {
     return (
         <button
-            className="absolute top-4 right-4 px-2 py-2 rounded-2xl bg-blue-300 cursor-pointer">
-            <BsPerson size={48} />
+            className="absolute top-4 right-4 px-2 py-2 rounded-2xl bg-blue-950 cursor-pointer">
+            <BsFillPersonFill size={48} className='text-blue-100'/>
         </button>
     );
         

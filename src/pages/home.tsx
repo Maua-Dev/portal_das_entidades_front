4240cd7536e393c02f3db68b81edff6f3c770be5
nikolasfunke <nikfunke1@gmail.com>
2025-10-17 12:52:58 -0300

    feat: add a path to the arrow

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,7 +2,7 @@
 import homeBackGround from "../assets/home-bg.jpg"
 import ProfileButton from "../components/profile";
 import ArrowButton from "../components/arrow"
-
+import { Link } from "react-router-dom";
 
 export default function Home() {
     // const { entityId } = useParams();
@@ -14,7 +14,9 @@ export default function Home() {
             style={{ backgroundImage: `url(${homeBackGround})` }}
         >
             <ProfileButton></ProfileButton>
-            <ArrowButton></ArrowButton>
+            <Link to='/entidades'>
+                <ArrowButton></ArrowButton>
+            </Link>
         </div>
     );
 }

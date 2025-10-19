99466cc442618265d84a5771dba0cbd6d2923b28
nikolasfunke <nikfunke1@gmail.com>
2025-10-19 18:05:06 -0300

    feat: add exprort button

--- src/components/export-button.tsx
+++ src/components/export-button.tsx
@@ -1,27 +1,27 @@
-import { HiDownload } from 'react-icons/hi';
+import { HiUpload } from 'react-icons/hi';
 
-interface ImportButtonProps {
+interface ExportButtonProps {
   label?: string;
   className?: string;
   iconSize?: number;
 }
 
 
-export default function ImportButton({
-  label = "Importar",
+export default function ExportButton({
+  label = "Exportar",
   className = "",
   iconSize = 20,
-}: ImportButtonProps) {
+}: ExportButtonProps) {
     return (
         <div className={`inline-flex items-center gap-1 ${className} bg-gray-400 px-2 rounded-2xl`}>
               <span className="text-sm font-medium text-white">{label}</span>
-
+        
               <button
                 type="button"
                 className="py-1 pl-1 rounded-full text-white active:scale-95 transition"
                 aria-label={label}
               >
-                <HiDownload size={iconSize} />
+                <HiUpload size={iconSize} />
               </button>
             </div>
     );

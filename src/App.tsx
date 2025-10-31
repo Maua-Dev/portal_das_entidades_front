5dcca074f339115644f2cde96522c527354468f7
tokujiTO <tiagomassuda123@gmail.com>
2025-10-31 18:26:06 -0300

    feat: add microsoft auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Entities from "./pages/entities.tsx";
 import Home from "./pages/home.tsx";
+import { msalInstance } from "./auth/auth-config.ts";
+import { MsalProvider } from "@azure/msal-react";
 
 export default function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/entidades" element={<Entities />} />
-        <Route path="/home/:entityId" element={<Home />}></Route>
-      </Routes>
+      <MsalProvider instance={msalInstance}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/entidades" element={<Entities />} />
+          <Route path="/home/:entityId" element={<Home />}></Route>
+        </Routes>
+      </MsalProvider>
     </div>
   );
 }
-
-
